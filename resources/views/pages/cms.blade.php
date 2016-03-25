0a3c8cb157519fec64baf825fd53b34ff2957538
luukspierings <lcpspier@avans.nl>
2016-03-25 12:21:33 +0100

    CMS view

---
+++ resources/views/pages/cms.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+
+                    <div class="panel-body">
+                        Dit is de CMS pagina.
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
