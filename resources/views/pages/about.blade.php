7352ca55815b16efc0c5c0bc99ce7354cba3473e
luukspierings <lcpspier@avans.nl>
2016-03-31 21:41:54 +0200

    Urenregistratie

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,14 +3,27 @@
 @section('content')
     <div class="container page-wrap">
         <div class="row">
-            <div class="col-md-10 col-md-offset-1">
+            <div class="col-md-12 col-md-offset-1">
                 <div class="panel panel-default">
                     <div class="panel-heading">Over ons</div>
 
                     <div class="panel-body">
-                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.
 
-                        Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia.
+                        <div>
+                            <h3>Luuk Spierings</h3>
+                            25-3  -  2 uur  -  Opzetten van project/Database <br>
+                            28-3  -  4 uur  -  Maken van CMS producten laden en opslaan<br>
+                            29-3  -  9 uur  -  Wijzigen/verwijderen van producten in CMS<br>
+                            30-3  -  10 uur  -  Lijst weergeven/aanmaken/wijzigen/verwijderen van categorieën in CMS<br>
+                            31-3  -  12 uur  -  Orders weergeven/verwijderen in CMS + filteren van categorieën + stijl veranderingen<br>
+                        </div>
+                        <div>
+                            <h3>Jonathan Immink</h3>
+
+
+                        </div>
+
+
                     </div>
                 </div>
             </div>
